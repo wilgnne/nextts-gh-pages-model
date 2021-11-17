7e5a361f247143cf378424822939238a4327f862
wilgnne <wilgnne.kba@gmail.com>
2021-11-17 10:04:35 +0000

    Deploying to gh-pages from  @ cca860d6283d318644f23cd035a757a9b410497f 🚀

---
+++ _next/static/t5uWqWp-aWx8IP52Z6gWm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-f4a2a266b14a0c07ff80.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
